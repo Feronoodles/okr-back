e4b566a129620e7994cbdc085540da92e7750381
Feronoodles <ferourlich@gmail.com>
2026-01-26 21:17:49 -0500

    fix: cors 2.0

--- src/main/java/com/example/okr/controller/KeyResultController.java
+++ src/main/java/com/example/okr/controller/KeyResultController.java
@@ -30,7 +30,7 @@ import java.util.List;
 @RequestMapping("/key_result")
 @AllArgsConstructor
 @SecurityRequirement(name = "bearer-key")
-@CrossOrigin(origins = "https://startling-genie-30d1f7.netlify.app/") // Permite peticiones de cualquier origen
+@CrossOrigin(origins = "https://startling-genie-30d1f7.netlify.app") // Permite peticiones de cualquier origen
 public class KeyResultController {
 
     private IKeyResultService iKeyResultService;
